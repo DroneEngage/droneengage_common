4817b496e384d8581489fc7229d2c06c07bc04d9
HefnySco <mohammad.hefny@gmail.com>
2025-10-29 18:33:44 +0300

    feature: parseDefaultCommand and moving handling TYPE_AndruavMessage_CONFIG_ACTION in common module

--- de_databus/de_facade_base.hpp
+++ de_databus/de_facade_base.hpp
@@ -1,7 +1,6 @@
 #ifndef P2P_ESP32_FACADE_BASE_H_
 #define P2P_ESP32_FACADE_BASE_H_
 
-
 #include <iostream>
 
 #include "../helpers/json_nlohmann.hpp"
@@ -12,22 +11,28 @@ using Json_de = nlohmann::json;
 
 namespace de
 {
-namespace comm
-{
-    class CFacade_Base
+    namespace comm
     {
+        class CFacade_Base
+        {
+
         public:
+            static CFacade_Base &getInstance()
+            {
+                static CFacade_Base instance;
 
-            void requestID(const std::string&target_party_id) const;
-            void sendErrorMessage (const std::string&target_party_id, const int& error_number, const int& info_type, const int& notification_type, const std::string& description) const;
+                return instance;
+            }
 
-            void API_sendConfigTemplate(const std::string&  target_party_id, const std::string& module_key, const Json_de& json_file_content_json, const bool reply);
-  
-        
+        public:
+            void requestID(const std::string &target_party_id) const;
+            void sendErrorMessage(const std::string &target_party_id, const int &error_number, const int &info_type, const int &notification_type, const std::string &description) const;
+
+            void API_sendConfigTemplate(const std::string &target_party_id, const std::string &module_key, const Json_de &json_file_content_json, const bool reply);
+
         protected:
-
-            CModule &m_module = de::comm::CModule::getInstance();            
-    };
-}
+            CModule &m_module = de::comm::CModule::getInstance();
+        };
+    }
 }
 #endif
--- de_databus/de_message_parser_base.hpp
+++ de_databus/de_message_parser_base.hpp
@@ -1,34 +1,38 @@
 #ifndef ANDRUAV_MESSAGE_PARSER_BASE_H_
 #define ANDRUAV_MESSAGE_PARSER_BASE_H_
 
+#include "de_message_parser_base.hpp"
+#include "de_facade_base.hpp"
+
 #include "../helpers/json_nlohmann.hpp"
 using Json_de = nlohmann::json;
 
 namespace de
 {
-namespace comm
-{
-    class CAndruavMessageParserBase
+    namespace comm
     {
-    public:
-        virtual ~CAndruavMessageParserBase() {}
+        class CAndruavMessageParserBase
+        {
+        public:
+            virtual ~CAndruavMessageParserBase() {}
 
-        void parseMessage(Json_de &andruav_message, const char *full_message, const int &full_message_length);
-        
-    protected:
-    
-        virtual void parseRemoteExecute(Json_de &andruav_message) = 0;
-        virtual void parseCommand(Json_de &andruav_message, const char *full_message, const int &full_message_length, int messageType, uint32_t permission) = 0;
+            void parseMessage(Json_de &andruav_message, const char *full_message, const int &full_message_length);
 
-    
-    protected:
+        protected:
+            virtual void parseRemoteExecute(Json_de &andruav_message) = 0;
+            virtual void parseCommand(Json_de &andruav_message, const char *full_message, const int &full_message_length, int messageType, uint32_t permission) = 0;
 
-        bool m_is_binary;
-        bool m_is_system;
-        bool m_is_inter_module;
+        private:
+            void parseDefaultCommand(Json_de &andruav_message, const char *full_message, const int &full_message_length, int messageType, uint32_t permission);
 
-    };
-}
+        protected:
+            bool m_is_binary;
+            bool m_is_system;
+            bool m_is_inter_module;
+
+            de::comm::CFacade_Base &m_facade = de::comm::CFacade_Base::getInstance();
+        };
+    }
 }
 
 #endif
